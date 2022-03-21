d52e1a2e32ded34f83b4903109c83e63148f6979
stenlyadam <stenly.adam@unklab.ac.id>
2022-03-21 16:10:40 +0800

    slicing splash screen

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
-import warnOnce from 'react-native/Libraries/Utilities/warnOnce';
-import Content from './components/Content';
-import Header from './components/Header';
-import Home from './screens/Home';
-import Register from './screens/Register';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import SplashScreen from './pages/SplashScreen';
 
 const App = () => {
-  const [welcome, setWelcome] = useState('Welcome');
-
-  const handleClick = () => {
-    setWelcome('Selamat Datang');
-  };
-
-  return (
-    // {/* <Text style={styles.welcome}>{welcome}</Text>
-    // <Header title="Dashboard" subTitle="Ini adalah halaman dashboard" />
-    // <Header title="Products" subTitle="Ini adalah halaman products" />
-    // <Content>
-    //   <Text>Hello World</Text>
-    // </Content>
-    // <Button title="Press Me" onPress={handleClick} /> */}
-    // {/* <Register /> */}
-    // <Register />
-    <Home />
-  );
+  return <SplashScreen />;
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  welcome: {
-    fontSize: 24,
-    color: 'black',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,27 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Logo from '../../assets/icons/Logo.svg';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Text style={styles.text}>Money Tracker</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#02CF8E',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-Regular',
+  },
+});
